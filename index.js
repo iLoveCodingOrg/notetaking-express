da192f4e3c4cbd5302611fd428d2b4eedd47b1f1
Aziz Ali <aziz@ilovecoding.org>
2018-03-17 14:03:04 -0500

    body parser added

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const PORT = 4000
 
 const noteRoutes = require('./notes/routes')
 const userRoutes = require('./users/routes')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/notes', noteRoutes)
 app.use('/users', userRoutes)
