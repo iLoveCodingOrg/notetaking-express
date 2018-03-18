894b7a803dc65c831422bf315a5c019eb9f5d1fb
Aziz Ali <aziz@ilovecoding.org>
2018-03-17 20:39:09 -0500

    user & modes schema and model created

---
+++ users/model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createDate: {
+    type: Date,
+    required: true,
+    default: new Date()
+  }
+})
+
+const UserModel = mongoose.model('User', userSchema)
+
+module.exports = UserModel
