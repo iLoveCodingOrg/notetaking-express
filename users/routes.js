426e4ea1e4e4b75f5f049cea7c5ef9d4ce935fe9
Aziz Ali <aziz@ilovecoding.org>
2018-03-18 14:30:11 -0500

    register input validation

--- users/routes.js
+++ users/routes.js
@@ -5,30 +5,32 @@ router.post('/login', (req, res, next)=>{
   res.send('login')
 })
 
-router.post('/register', (req, res, next)=>{
-  const newUser = new UserModel({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password
-  })
-
-  newUser
-    .save()
-    .then((document)=>{
-      if(document){
-        document.password = undefined
-        res.json(document)
-      }else{
-        res.send('document did not save')
-      }
-    })
-    .catch((err)=>{
-      console.log(err)
-      res.send('error happened')
+router.post('/register',
+  registerInputValidation,
+  (req, res, next)=>{
+    const newUser = new UserModel({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password
     })
 
-})
+    newUser
+      .save()
+      .then((document)=>{
+        if(document){
+          document.password = undefined
+          res.json(document)
+        }else{
+          res.send('document did not save')
+        }
+      })
+      .catch((err)=>{
+        console.log(err)
+        res.send('error happened')
+      })
+  }
+)
 
 router.get('/:id', (req, res, next)=>{
   UserModel
@@ -49,4 +51,29 @@ router.get('/:id', (req, res, next)=>{
     })
 })
 
+function registerInputValidation(req, res, next){
+  const { firstName, lastName, email, password } = req.body
+  const missingFileds = []
+
+  if(!firstName){
+    missingFileds.push('firstName')
+  }
+  if(!lastName){
+    missingFileds.push('lastName')
+  }
+  if(!email){
+    missingFileds.push('email')
+  }
+  if(!password){
+    missingFileds.push('password')
+  }
+
+  if(missingFileds.length){
+    res
+      .status(400)
+      .send(`The following fields are missing: ${missingFileds.join(', ')}`)
+  }else{
+    next()
+  }
+}
 module.exports = router
